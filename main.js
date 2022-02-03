2f5e50d6882198f8e0e4dbaa356e4ce5263cc355
atharva3012 <92358033+atharva3012@users.noreply.github.com>
2022-02-03 15:24:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function classifyCanvas(){
+    
+}
